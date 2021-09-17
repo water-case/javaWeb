0297aaaa001c98b21964667c5ea5a21cf494b14a
water-case <65841586+water-case@users.noreply.github.com>
2021-09-17 18:20:51 +0900

    210917 18:20

---
+++ pro05/src/main/java/ex01/ThirdServlet.java
@@ -0,0 +1,30 @@
+package ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ThirdServlet init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("ThirdServlet destroy 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doPost 메서드 호출");
+	}
+
+}
